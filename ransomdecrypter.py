e3d6e20bbeb973a8894c046a1ed2501b71787215
cavid00 <116761465+cavid00@users.noreply.github.com>
2023-01-16 03:33:44 +0400

    Add files via upload

---
+++ ransomdecrypter.py
@@ -0,0 +1,24 @@
+import os
+from cryptography.fernet import Fernet
+
+files = []
+
+for file in os.listdir():
+	if file == "ransom.py" or file == "generatedkey.key" or file =="ransomdecrypter.py":
+		continue #do not encrypt the current file we are working with or the key
+	if os.path.isfile(file):
+		files.append(file) #do not add if this is a directory, only if a file
+
+for i in files:
+	print(i)
+print("Passwords are broken :)")
+
+with open("generatedkey.key","rb") as generatedkey:
+	secret_key = generatedkey.read()
+
+for file in files:
+	with open(file,"rb") as the_file:
+		contents = the_file.read()
+	contents_decrypted = Fernet(secret_key).decrypt(contents)
+	with open(file,"wb") as the_file:
+		the_file.write(contents_decrypted)
